f7e86124080c9fcb13f4ab3aeeebba8fe4e59faf
ImLJS <109210868+ImLJS@users.noreply.github.com>
2025-02-06 03:17:40 +0530

    init

---
+++ src/app/providers.tsx
@@ -0,0 +1,10 @@
+import { ThemeProvider } from 'next-themes';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+            {children}
+        </ThemeProvider>
+    );
+};
+export default Providers;
